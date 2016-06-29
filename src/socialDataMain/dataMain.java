ae529460385cf924e99b9df8773ff31ba082e068
njudislab-lyz <602298461@qq.com>
2016-06-29 22:03:02 +0800

    add file

--- src/socialDataMain/dataMain.java
+++ src/socialDataMain/dataMain.java
@@ -1,7 +1,21 @@
 package socialDataMain;
-//import java.io.*;
+import java.io.*;
+import java.util.*;
 public class dataMain{
-	public static void main(String []args){
+	@SuppressWarnings("resource")
+	public static void main(String []args) throws FileNotFoundException{
 		System.out.println("I back for Java!");
+		File netDataIn = new File("twitterData\\follower_gcc.anony.dat");
+		if(netDataIn.exists()){
+			Scanner netInput = new Scanner(netDataIn);
+			while(netInput.hasNextLine()){
+				String s = netInput.nextLine();
+				System.out.println(s);
+			}
+		}
+		else {
+			System.out.println("net data file path wrong !" +netDataIn.getAbsolutePath());
+			return;
+		}
 	}
 }
