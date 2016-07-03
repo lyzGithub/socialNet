f3827d177bd215cf4b4f4852a42f24098c78f342
njudislab-lyz <602298461@qq.com>
2016-07-03 12:53:25 +0800

    directed graph test

--- src/socialDataMain/dataMain.java
+++ src/socialDataMain/dataMain.java
@@ -2,47 +2,73 @@ package socialDataMain;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.net.*;
-
 import org.jgrapht.*;
+import org.jgrapht.WeightedGraph;
 import org.jgrapht.graph.*;
 
 public class dataMain{
 	@SuppressWarnings("resource")
 	public static void main(String []args) throws FileNotFoundException{
 		// create a menber for undirectedGraph
-		UndirectedGraph<String, DefaultEdge> netBaseG =
-	            new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+		DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> netBaseDireG =
+	            new DefaultDirectedWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
 	
-		System.out.println("Please wait, laoding data!!");
-		File netDataIn = new File("twitterData\\follower_gcc.anony.dat");
 		
-        
-		if(netDataIn.exists()){
+		System.out.println("Please wait, laoding data!!");
+		File netDataIn = new File("twitterData\\testNet.txt");//("twitterData\\follower_gcc.anony.dat");
+		File edgeDataIn = new File("twitterData\\testEdgeInfo.txt");
+		if(netDataIn.exists() && edgeDataIn.exists()){ 
 			// add the vertices and edges
-			Scanner netVerInput = new Scanner(netDataIn);
 			long startTime = System.currentTimeMillis();
+			Scanner netVerInput = new Scanner(netDataIn);
 			while(netVerInput.hasNextLine()){
 				String s = netVerInput.nextLine();
 				String[] ss = s.split(" ");
-				if(false == netBaseG.containsVertex(ss[0])){
-					netBaseG.addVertex(ss[0]);
+				//add vertexes
+				if(false == netBaseDireG.containsVertex(ss[0])){
+					netBaseDireG.addVertex(ss[0]);
 				}
-				if(false == netBaseG.containsVertex(ss[1])){
-					netBaseG.addVertex(ss[1]);
+				if(false == netBaseDireG.containsVertex(ss[1])){
+					netBaseDireG.addVertex(ss[1]);
 				}
-				netBaseG.addEdge(ss[0], ss[1]);
+				//netBaseDireG.addEdge(ss[0], ss[1]);
+				//DefaultWeightedEdge we = new DefaultWeightedEdge();
+				netBaseDireG.addEdge(ss[0], ss[1]);
+				netBaseDireG.addEdge(ss[1], ss[0]);
 				
 			}
-			long endTime = System.currentTimeMillis();
 			netVerInput.close();
-			System.out.println("Graph built! TimeMillis spend: " + (endTime-startTime));
+			long endTime = System.currentTimeMillis();
+			System.out.println("Graph built! TimeMillis spend: " + (endTime-startTime) + " ms.");
+			
+			//set weight for edges
+			startTime = System.currentTimeMillis();
+			Scanner edgeInfoInput = new Scanner(edgeDataIn);
+			while(edgeInfoInput.hasNextLine()){
+				String s = edgeInfoInput.nextLine();
+				String[] ss = s.split(" ");
+				try{
+					netBaseDireG.setEdgeWeight(netBaseDireG.getEdge(ss[0], ss[1]), Integer.parseInt(ss[2]));
+					netBaseDireG.setEdgeWeight(netBaseDireG.getEdge(ss[1], ss[0]), Integer.parseInt(ss[2]));
+				}
+				catch(Exception e){
+					System.out.println(e.toString());
+				}
+			}
+			endTime = System.currentTimeMillis();
+			System.out.println("Graph edges weight seted! TimeMillis spend: " + (endTime-startTime) + " ms.");
+			//algorithm for possibility
+			startTime = System.currentTimeMillis();
+			
+			endTime = System.currentTimeMillis();
+			System.out.println("Compute possibility for each edges to translate info! TimeMillis spend: " + (endTime-startTime) + " ms.");
 		}
 		else {
 			System.out.println("net data file path wrong !" +netDataIn.getAbsolutePath());
 			return;
 		}
-		//System.out.println(netBaseG.toString());
+		System.out.println(netBaseDireG.toString());
+		
 	}
 	
 	
